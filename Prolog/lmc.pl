7effb6e110f2b054a157f5764bc875605928c310
bigboss98 <marco_natali@ymail.com>
2018-11-28 17:10:45 +0100

    Modificato ed implementate le varie operazioni

--- Prolog/lmc.pl
+++ Prolog/lmc.pl
@@ -1,97 +1,131 @@
 %%%% -*- Mode:Prolog -*-
 
-% one_instruction(State, NewState) serve per simulare/eseguire un'istruzione del LMC
-% Prevede che State e NewState siano stati validi del LMC  e comporta l'esecuzione
-% dell'istruzione che porta da State a NewState.
-% Il predicato fallisce nei seguenti casi:
-% - lo stato State è un halting_state, ossia il sistema si è arrestato
-% - l'istruzioni è di input ma la coda di input è vuota
-% - l'istruzione corrente non è valida.
-one_instruction(state(Acc, Pc, Mem, In, Out, Flag),
-                state(NewAcc, NewPc, Mem, In, Out, NewFlag)):-
-        execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
-                              state(NewAcc, NewPc, NewMem, NewIn, NewOut, NewFlag)).
+/* one_instruction(State, NewState) serve per simulare/eseguire un'istruzione del LMC
+ * Prevede che State e NewState siano stati validi del LMC  e comporta l'esecuzione
+ * dell'istruzione che porta da State a NewState.
+ * Il predicato fallisce nei seguenti casi:
+ * - l'istruzioni è di input ma la coda di input è vuota
+ * - l'istruzione corrente non è valida.
+ */
+one_instruction(State, NewState):-
+        execution_instruction(State, NewState).
 
-/*one_instruction(state(Acc, Pc, Mem, In, Out, Flag),
-                halted_state(NewAcc, NewPc, NewMem, NewIn, NewOut, NewFlag):-
-                  nth0().
-*/
+/* Predicato execution_loop/2 per eseguire una sequenza di istruzioni 
+ *
+ */
+execution_loop(halted_state(Acc, Pc, Mem, In, Out, Flag), Out).
+
+execution_loop(State, Out):-
+               one_instruction(State, NewState),
+               execution_loop(NewState, Out).
+
 
 /*execution_instruction/2 permette di eseguire le istruzioni del linguaggio macchina
- * del LMC, ritorna un errore in caso l'istruzione non sia valida oppure si ha
- * la presenza di un istruzione halt.
+ * del LMC.
  */
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(NewAcc, NewPc, Mem, In, Out, NewFlag)):-
             nth0(Pc, Mem, Elem, R),%prelevo l'instruzione corrente dalla memoria
-            write(Elem / 100 is 1), !, %isolo il primo elemento per sapere la tipologia di istruzione
-		        write(Elem mod 100 is X),
-            sum(X, Acc, NewAcc, NewFlag),
-            NewPc is Pc + 1. %aggiorno il Program Counter
+            1 is Elem div 100, !, %isolo il primo elemento per sapere la tipologia di istruzione
+		        X is Elem mod 100,
+            nth0(X, Mem, Num, R1),
+            sum(Num, Acc, NewAcc, NewFlag),
+            NewPc is Pc + 1 mod 100. 
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(NewAcc, NewPc, Mem, In, Out, NewFlag)):-
             nth0(Pc, Mem, Elem, R),%prelevo l'instruzione corrente dalla memoria
-            Elem / 100 is 2, !, %isolo il primo elemento per sapere la tipologia di istruzione
-            X is Elem mod 100,
-            diff(X, Acc, NewAcc, NewFlag),
-            NewPc is Pc + 1. %aggiorno il Program Counter
+            2 is (Elem div 100), !,
+            X is (Elem mod 100),
+            nth0(X, Mem, Num, R1),
+            diff(Num, Acc, NewAcc, NewFlag),
+            NewPc is (Pc + 1) mod 100.
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(Acc, NewPc, NewMem, In, Out, Flag)):-
                       nth0(Pc, Mem, Elem, R),
-                      Elem / 100 is 3, !,
-                      Elem mod 100 is X,
+                      3 is Elem / 100, !,
+                      X is Elem mod 100,
                       store(X, Acc, Mem, NewMem),
-                      NewPC is Pc + 1.
+                      NewPc is (Pc + 1) mod 100.
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(NewAcc, NewPc, Mem, In, Out, Flag)):-
                       nth0(Pc, Mem, Elem, R),
-                      Elem / 100 is 5, !,
-                      Elem mod 100 is X,
-                      load(X, Acc, Mem, NewAcc).
+                      5 is Elem / 100, !,
+                      X is Elem mod 100,
+                      nth0(X, Mem, Num, R1),
+                      load(Num, Acc, Mem, NewAcc),
+                      NewPc is (Pc + 1) mod 100.
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(Acc, NewPc, Mem, In, Out, Flag)):-
                       nth0(Pc, Mem, Elem, R),
-                      Elem / 100 is 6, !,
-                      Elem mod 100 is X,
-                      branch(X, NewPc).
+                      6 is Elem / 100, !,
+                      X is Elem mod 100,
+                      nth0(X, Mem, Num, R1),
+                      branch(Num, NewPc).
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(Acc, NewPc, Mem, In, Out, Flag)):-
                       nth0(Pc, Mem, Elem, R),
-                      Elem / 100 is 7, !,
-                      Elem mod 100 is X,
-                      % .
+                      7 is Elem / 100, !,
+                      X is Elem mod 100,
+                      nth0(X, Mem, Num, R1),
+                      NewPc is Pc + 1,%aggiorno il Program Counter
+                      branch_zero(Num, Acc, Flag, NewPc).
+
+execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
+                      state(Acc, NewPc, Mem, In, Out, Flag)):-
+                      nth0(Pc, Mem, Elem, R),
+                      8 is Elem / 100, !,
+                      X is Elem mod 100,
+                      nth0(X, Mem, Num, R1),
+                      NewPc is Pc + 1,%aggiorno il Program Counter
+                      branch_positive(Num, Acc, Flag, NewPc).
+
+execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
+                      state(Acc, Pc, Mem, NewIn, Out, Flag)):-
+                      nth0(Pc, Mem, Elem, R),
+                      901 is Elem, !,
+                      input(In, Acc, NewIn),
+                      NewPc is (Pc + 1) mod 100.
+
+execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
+                      state(Acc, Pc, Mem, In, NewOut)):-
+                      nth0(Pc, Mem, Elem, R),
+                      902 is Elem, !,
+                      output(Acc, Out, NewOut),
+                      NewPc is (Pc + 1) mod 100.
+
+execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
+                      halted_state(Acc, Pc, Mem, In, Out, Flag)):-
+                      nth0(Pc, Mem, Elem, R),
+                      Elem / 100 is 0.
+                      
 /*Il predicato sum/4 permette l'implementazione della somma tra un registro e
  * l'accumulatore e salva il risultato ,modulo 1000 nell'accumulatore.
  * In caso la somma sia maggiore di 1000 viene settato il Flag altrimenti no.
  */
-sum(Elem, Acc, NewAcc, Flag):-
+sum(Elem, Acc, NewAcc, flag):-
           Result is (Elem + Acc),
           Result >= 1000, !,
-          Flag = string("flag"),
           NewAcc is Result mod 1000.
 
-sum(Elem, Acc, NewAcc, Flag):-
-          NewAcc is (Elem + Acc),
-          Flag = string("noflag").
-
-
+sum(Elem, Acc, NewAcc, noflag):-
+          NewAcc is (Elem + Acc), !.
 
 %Predicato diff/4 per implementare la sottrazione tra un registro e l'accumulatore
 % e salva il risultato nell'accumulatore
-diff(Elem, Acc, NewAcc, Flag):-
+diff(Elem, Acc, NewAcc, NewFlag):-
         Result is (Elem - Acc),
         Result < 0, !,
-        Flag = string("flag"),
+        NewFlag = string("flag"),
         NewAcc is Result mod 1000.
 
 diff(Elem, Acc, NewAcc, Flag):-
-        NewAcc is (Elem - Acc),
-        Flag = string("noflag").
+        NewAcc is (Elem - Acc), !,
+        NewFlag = string("noflag").
 
 /*predicato store/4 per salvare nella cella di memoria di indirizzo Elem,
  *il valore contenuto nell'accumulatore Acc e rimane inalterato l'accumulatore.
@@ -109,3 +143,29 @@ diff(Elem, Acc, NewAcc, Flag):-
  * impostato il Program Counter al valore passato come argomento chiamato Elem
  */
  branch(Elem, Elem).
+
+/* Predicato branch_zero/4 per effettuare un salto condizionato in caso in cui
+ * il valore nell'accumulatore sia 0 e il flag risulta assente.
+ * Il valore del Program Counter viene posto al valore Elem passato in argomento.
+ */
+ branch_zero(Elem, Acc, noflag, Pc):-
+             Acc is 0,
+             Pc is Elem.
+
+/* Predicato branch_positive/4 per effettuare un salto condizionato in caso il valore
+ * nell'accumulatore sia maggiore di 0 e il flag risulta assente.
+ * Il valore del PC viene posto uguale all'elemento passato come argomento.
+ */
+branch_positive(Elem, Acc, Flag, Pc):-
+             Acc > 0,
+             Flag = string("noflag"),
+             Pc is Elem.
+
+/* Predicato input/3 per effettuare un operazione di input
+ */
+input(In, Acc, NewIn):-
+      nth0(0, In, Elem, NewIn),
+      Acc is Elem.
+
+output(Acc, Out, [Out | Acc]).
+       
