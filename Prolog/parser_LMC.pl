af5a40e13d2b471bde827a3ca87e7427b2a62678
dearcoding <giovanniocchiuto3@gmail.com>
2018-12-01 18:46:03 +0100

    Parser LMC, senza etichette implementate

--- Prolog/parser_LMC.pl
+++ Prolog/parser_LMC.pl
@@ -1,8 +1,8 @@
 %%%% -*- Mode: Prolog -*-
 
-%%%% parse-stuff.pl
+%%%% parse-LMC.pl
 %%%%
-%%%% Remember that you are always really "following" a state machine.
+%%%% A simple LMC parser written in prolog.
 
 
 %%% is_digit/1
@@ -77,21 +77,156 @@ parse_string([C | Cs], DigitsSoFar, I, DigitCodes, [C | Cs]) :-
     reverse(DigitsSoFar, DigitCodes),
     string_codes(I, DigitCodes).
 
+%%% lmc_load/2
+%%% lmc_load(Filename, Mem)
+%%% The goal reads the content of "Filename" and converts it in ASCII code
+%%% Then it starts the assembly parsing using parse_ass/2 predicate.
+
 lmc_load(Filename, Mem) :-
     read_file_to_codes(Filename, Code, []),
     parse_ass(Code, Mem).
 
+
+%%% parse_ass/2
+%%% parse_ass(Code, Mem)
+%%% The goal cares of building the Memory list.
+
 parse_ass([], []) :- !.
 
-parse_ass(Code, [Y|Mem]) :-
-    parse_op(Code, Y, Rest),
+%%% That is the case a line starts with "\\"
+
+parse_ass([X, X | Code], Mem) :-
+    X = 92,!,
+    parse_end([X, X | Code], Codes),
+    parse_ass(Codes, Mem).
+
+parse_ass(Code, [Y | Mem]) :-
+    parse_op(Code, Y, Others),
+    parse_end(Others, Rest),
     parse_ass(Rest, Mem).
 
-parse_op(Code, Y, Rest) :-
-    parse_string(Code, Y, Rest),
-    Y \= "",
-    string_upper(Y, YY),
-    YY = "ADD",
-    parse_int(Rest, Y, Rest),
+%%% parse_end_line_comment/2
+%%% parse_end_line_comment(Code, Codes)
+%%% It deletes the comment from a line, till it finds "\n" (10 in ASCII)
 
-%%%% end of file -- parse-stuff.pl
+parse_end_line_comment([], []):- !.
+
+parse_end_line_comment([X], []):- X = 10, !.
+
+parse_end_line_comment([X | Z], Z):-
+    X = 10, !.
+
+parse_end_line_comment([X | Z], Zs) :-
+    X \= 10,
+    parse_end_line_comment(Z, Zs), !.
+
+
+%%% parse_end/2
+%%% parse_end(Code, Codes)
+%%% It checks the end of a line, it deletes whitespace (32 in ASCII)
+%%% however it finds "\n" (10 in ASCII) it breaks.
+
+parse_end([],[]) :- !.
+
+parse_end([First, Second | Body], Rest) :-
+    First = 92, Second = 92, !,
+    parse_end_line_comment(Body, Rest).
+
+parse_end([X | Body], Body) :-
+    X = 10, !.
+
+parse_end([X | Body], Rest) :-
+    X = 32,
+    parse_end(Body, Rest).
+
+%%% parse_op/3
+%%% parse_op(Code, AssCode, Rest)
+%%% The goal reads the list of ascii codes, and however it finds a
+%%% correct instruction it returns the equivalent assembly code.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "ADD", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 100.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "SUB", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 200.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "STA",!,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 300.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "LDA", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 500.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "BRA", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 600.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "BRZ", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 700.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "BRP", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 800.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Other),
+    string_upper(String, StringUp),
+    StringUp = "BRP", !,
+    parse_int(Other, MemValue, Rest),
+    MemValue < 100, !, MemValue > 0, !,
+    AssCode is MemValue + 800.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Rest),
+    string_upper(String, StringUp),
+    StringUp = "INP", !,
+    AssCode is 901.
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Rest),
+    string_upper(String, StringUp),
+    StringUp = "OUT", !,
+    AssCode is 902.
+
+
+parse_op(Code, AssCode, Rest) :-
+    parse_string(Code, String, Rest),
+    string_upper(String, StringUp),
+    StringUp = "HLT", !,
+    AssCode is 0.
+
+parser
+
+%%%% end of file -- parse-LMC.pl
