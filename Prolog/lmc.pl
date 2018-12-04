6ab08323fbe97236eb5db682d7e697ac2b30da4e
bigboss98 <marco_natali@ymail.com>
2018-12-04 11:52:44 +0100

    Effettuati tutti i test

--- Prolog/lmc.pl
+++ Prolog/lmc.pl
@@ -10,8 +10,9 @@
  * In caso in cui viene eseguita un'istruzione di halt NewState è di tipo halted_state
  * altrimenti NewState è di tipo state.
  */
-one_instruction(State, NewState):-
-        execution_instruction(State, NewState).
+one_instruction(state(Acc, Pc, Mem, In, Out, Flag), NewState):-
+        \+ length(Mem, 0),
+        execution_instruction(state(Acc, Pc, Mem, In, Out, Flag), NewState).
 
 /* 
  * Predicato execution_loop/2(State, Out) per eseguire una sequenza di istruzioni 
@@ -21,11 +22,14 @@ one_instruction(State, NewState):-
  * - la seconda prevede di eseguire una certa serie di istruzioni fino a quando State è di tipo state
  *   e alla fine dell'esecuzione viene ritornata la coda di output 
 */
-execution_loop(halted_state(Acc, Pc, Mem, In, Out, Flag), Out).
+execution_loop(halted_state(Acc, Pc, Mem, In, Out, Flag), Out):- !.
 
 execution_loop(state(Acc, Pc, Mem, In, Out, Flag), Out):-
-               one_instruction(state(Acc, Pc, Mem, In, Out, Flag), NewState),
-               execution_loop(NewState, Out).
+              %length(Mem, 100),
+              between(0 ,99, Pc), !,
+              write("Esecuzione numero:"), write(Pc),
+              one_instruction(state(Acc, Pc, Mem, In, Out, Flag), NewState),
+              execution_loop(NewState, Out).
 
 
 /* 
@@ -38,6 +42,7 @@ execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(NewAcc, NewPc, Mem, In, Out, NewFlag)):-
           nth0(Pc, Mem, Elem, R),
           recognize_instruction(Elem, 1, Rest), !,
+          write(Elem: ADD),
           nth0(Rest, Mem, Num, R1), 
           sum(Num, Acc, NewAcc, NewFlag),
           NewPc is (Pc + 1) mod 100. 
@@ -46,6 +51,7 @@ execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(NewAcc, NewPc, Mem, In, Out, NewFlag)):-
           nth0(Pc, Mem, Elem, R),
           recognize_instruction(Elem, 2, Rest), !,
+          write(Elem: SUB),
           nth0(Rest, Mem, Num, R1),
           diff(Num, Acc, NewAcc, NewFlag),
           NewPc is (Pc + 1) mod 100.
@@ -53,15 +59,14 @@ execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(Acc, NewPc, NewMem, In, Out, Flag)):-
           nth0(Pc, Mem, Elem, R),
-          recognize_instruction(Elem, 3, Rest), !,
-          store(Rest, Acc, Mem, NewMem),
+          recognize_instruction(Elem, 3, Num), !,
+          store(Acc, Num, Mem, NewMem),
           NewPc is (Pc + 1) mod 100.
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(NewAcc, NewPc, Mem, In, Out, Flag)):-
           nth0(Pc, Mem, Elem, R),
-          recognize_instruction(Elem, 5, Rest), !,
-          nth0(Rest, Mem, Num, R1),
+          recognize_instruction(Elem, 5, Num), !,
           load(Num, Acc, Mem, NewAcc),
           NewPc is (Pc + 1) mod 100.
 
@@ -75,34 +80,33 @@ execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(Acc, NewPc, Mem, In, Out, Flag)):-
           nth0(Pc, Mem, Elem, R),
           recognize_instruction(Elem, 7, Num), !,
-          NewPc is (Pc + 1) mod 100,
-          branch_zero(Num, Acc, Flag, NewPc).
+          branch_zero(Num, Acc, Flag, Pc, NewPc).
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
                       state(Acc, NewPc, Mem, In, Out, Flag)):-
           nth0(Pc, Mem, Elem, R),
           recognize_instruction(Elem, 8, Num), !,
-          NewPc is (Pc + 1) mod 100,
-          branch_positive(Num, Acc, Flag, NewPc).
+          branch_positive(Num, Acc, Flag, Pc, NewPc).
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
-                      state(NewAcc, Pc, Mem, NewIn, Out, Flag)):-
+                      state(NewAcc, NewPc, Mem, NewIn, Out, Flag)):-
           nth0(Pc, Mem, Elem, R),
           recognize_instruction(Elem, 901), !,
           input(In, NewAcc, NewIn),
           NewPc is (Pc + 1) mod 100.
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
-                      state(Acc, Pc, Mem, In, NewOut)):-
+                      state(Acc, NewPc, Mem, In, NewOut, Flag)):-
           nth0(Pc, Mem, Elem, R),
           recognize_instruction(Elem, 902), !,
           output(Acc, Out, NewOut),
           NewPc is (Pc + 1) mod 100.
 
 execution_instruction(state(Acc, Pc, Mem, In, Out, Flag),
-                      halted_state(Acc, Pc, Mem, In, Out, Flag)):-
+                      halted_state(Acc, NewPc, Mem, In, Out, Flag)):-
           nth0(Pc, Mem, Elem, R),
-          recognize_instruction(Elem, 0).
+          recognize_instruction(Elem, 0, Num),
+          NewPc is (Pc + 1) mod 100.
                       
 /*
  * Predicato sum/4 implementa la somma tra un registro e l'accumulatore 
@@ -136,16 +140,15 @@ diff(Elem, Acc, NewAcc, noflag):-
  * Predicato store/4(Elem, Acc, Mem, NewMem) per salvare nella cella di memoria di indirizzo Elem,
  * il valore contenuto nell'accumulatore Acc, lasciando inalterato l'accumulatore.
  */
- store(Elem, Acc, Mem, NewMem):-
-        replace(Acc, Mem, Elem, NewMem).
+ store(Acc, Elem, Mem, NewMem):-
+        replace(Acc, Elem, Mem, NewMem).
 
 /* 
  * Predicato load/4(Elem, Acc,Mem, NewAcc) per prelevare un elemento dalla memoria e lo salva nell'
  * accumulatore, lasciando inalterato il contenuto in memoria.
  */
- load(Elem, Acc, Mem, NewAcc):-
-       nth0(Elem, Mem, NewAcc).
-
+ load(Index, Acc, Mem, NewAcc):-
+        nth0(Index, Mem, NewAcc).
 /* 
  * Predicato branch/2 per effettuare un salto non condizionale in cui viene
  * impostato il Program Counter al valore passato come argomento chiamato Elem
@@ -157,23 +160,23 @@ diff(Elem, Acc, NewAcc, noflag):-
  * il valore nell'accumulatore sia 0 e il flag risulta assente, ponendo il valore del Pc
  * uguale al valore di Elem.
  */
- branch_zero(Elem, 0, flag, Elem).
+ branch_zero(Elem, 0, noflag, Pc, NewPc):-
+             NewPc is Elem, !.
 
- branch_zero(Elem, Acc, Flag, NewPc):-
-             NewPc is (Pc + 1) mod 100.
+ branch_zero(Elem, Acc, Flag, Pc, NewPc):-
+            NewPc is (Pc +1) mod 100.
 
 /* 
  * Predicato branch_positive/4 per effettuare un salto condizionato in caso il valore
  * nell'accumulatore sia maggiore di 0 e il flag risulta assente, ponendo in tal caso 
  * il valore del PC uguale ad Elem.
  */
-branch_positive(Elem, Acc, Flag, Elem):-
-             Acc >= 0, !,
-             Flag is flag.
+branch_positive(Elem, Acc, noflag, Pc, Elem):-
+                Acc > 0, !.
 
-branch_positive(Elem, Acc, Flag, NewPc):-
-             \+ branch_positive(Elem, Acc, Flag, Elem),
-             NewPc is (Pc + 1) mod 100.
+branch_positive(Elem, Acc, Flag, Pc, NewPc):-
+            \+ (Acc > 0, Flag = string("noflag")), !,
+            NewPc is (Pc + 1) mod 100.
 
 /* 
  * Predicato input/3(In, NewAcc, NewIn) per effettuare un operazione di input
@@ -181,19 +184,21 @@ branch_positive(Elem, Acc, Flag, NewPc):-
  * In caso la coda sia vuota fallisce il predicato.
  */
 input(In, NewAcc, NewIn):-
+      \+ length(In, 0), !,
       nth0(0, In, NewAcc, NewIn).
 
 /*
  * Predicato output/3(Acc, Out, NewOut) effettua l'operazione di output
  * in cui salva il valore dell'accumulatore in fondo alla coda di output.
  */
-output(Acc, Out, [Out | Acc]).
+output(Acc, Out, NewOut):-
+       append([Acc], Out, NewOut).
    
 /*
  * Predicato recognize_instruction(Elem, Type, Rest) per riconoscere la tipologia
  * di istruzione e separare l'opcode dell'istruzione dal valore della cella di memoria dell'istruzione.
  */
-recognize_instruction(Elem, Type, Rest):-
+recognize_instruction(Elem, Type, Num):-
           Type is (Elem div 100), !,
           Num is (Elem mod 100).
 
@@ -205,8 +210,10 @@ recognize_instruction(Elem, Elem):-
  * Predicato replace/4(Num, Pos, NewMem) per cambiare il valore dell'elemento 
  * della lista Mem presente nella posizione Pos.
  */
-replace(Num, 0, [X | Xs], [Num | Xs]).
+replace(Num, 0, [X | Xs], [Num | Xs]):- !.
 
 replace(Num, Pos, [X | Xs], [X | Ys]):-
         NewPos is Pos - 1,
         replace(Num, NewPos, Xs, Ys).
+
+%%%% End of file lmc.pl
--- Prolog/lmc.plt
+++ Prolog/lmc.plt
@@ -8,11 +8,63 @@
  * Test per verificare che le istruzioni aritmetiche forniscono i risultati corretti
  */
  test(mathOperation):-
- 	  one_instruction(state(2, 0, [202, 23, 45], [], [], noflag), X),
-      one_instruction(state(3, 0, [101, 34], [], [], noflag), Y),
-      one_instruction(state(3, 0, [104, 34, 345, 45, 999], [], [], noflag), Z),
+ 	  lmc:one_instruction(state(2, 0, [202, 23, 45], [], [], noflag), X),
+      lmc:one_instruction(state(3, 0, [101, 34], [], [], noflag), Y),
+      lmc:one_instruction(state(3, 0, [104, 34, 345, 45, 999], [], [], noflag), Z),
       X = state(43, 1, [202, 23, 45], [], [], noflag),
       Y = state(37, 1, [101, 34], [], [], noflag),
       Z = state(2, 1, [104, 34, 345, 45, 999], [], [], flag).
  
+ test(memoryOperation):-
+      lmc:one_instruction(state(12, 0, [301, 0], [], [], noflag), Res1),
+      lmc:one_instruction(state(2, 0, [502, 34, 567], [], [], noflag), Res2),
+      %\+ lmc:one_instruction(state(4, 0, [503, 0, 45], [], [], noflag), X),%deve fallire perchè manca elemento in memoria
+      %\+ lmc:one_instruction(state(2, 0, [], [], [], noflag), Y), %deve fallire perchè la memoria è vuota
+      Res1 = state(12, 1, [301, 12], [], [], noflag),
+      Res2 = state(567, 1, [502, 34, 567], [], [], noflag).
+
+test(branch):-
+     lmc:one_instruction(state(3, 0, [603, 45, 678], [], [], noflag), Res1),
+     lmc:one_instruction(state(4, 0, [603], [], [], noflag), Res2),
+     Res1 = state(3, 3, [603, 45, 678], [], [], noflag),
+     Res2 = state(4, 3, [603], [], [], noflag),
+     lmc:one_instruction(state(2, 0, [703, 45, 456, 678], [], [], noflag), Res3),
+     lmc:one_instruction(state(0, 0, [703, 45], [], [], noflag), Res4),
+     lmc:one_instruction(state(0, 0, [703], [], [], flag), Res5),
+     lmc:one_instruction(state(0, 0, [803, 56], [], [], noflag), Res6),
+     lmc:one_instruction(state(2, 0, [803], [], [], flag), Res7),
+     lmc:one_instruction(state(2, 0, [804], [], [], noflag), Res8),
+     Res3 = state(2, 1, [703, 45, 456, 678], [], [], noflag),
+     Res4 = state(0, 3, [703, 45], [], [], noflag),
+     Res5 = state(0, 1, [703], [], [], flag),
+     Res6 = state(0, 1, [803, 56], [], [], noflag),
+     Res7 = state(2, 1, [803], [], [], flag),
+     Res8 = state(2, 4, [804], [], [], noflag).
+
+
+test(inputOutput):-
+     lmc:one_instruction(state(3, 0, [901], [456], [], noflag), Res1),  
+     lmc:one_instruction(state(45, 0, [902], [], [], noflag), Res2),
+     \+ lmc:one_instruction(state(3, 0, [901], [], [], noflag), X),  
+     Res2 = state(45, 1, [902], [], [45], noflag),
+     Res1 = state(456, 1, [901], [], [], noflag).
+     
+
+test(halt):-
+     lmc:one_instruction(state(3, 0, [045], [], [], noflag), Res1),
+     lmc:one_instruction(state(4, 0, [048], [], [], noflag), Res2),
+     Res1 = halted_state(3, 1, [45], [], [], noflag),
+     Res2 \= state(4, 1, [48], [], [], noflag).
+
+
+test(executionLoop):- 
+    \+ lmc:execution_loop(state(0, 0, [456, 234, 123], [], [], noflag), X),
+    lmc:execution_loop(state(0, 0, [901, 315, 901, 711, 214, 316, 517, 115, 516, 603, 517, 902, 0, 1, 0,
+                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [41, 42], [], noflag), Out1),
+    Out1 = [722].
+    
+
 :-end_tests(testLmc).
