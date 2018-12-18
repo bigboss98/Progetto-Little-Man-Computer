c3b3468f4122eccfa31a466ca4a8a07bd7c6a904
marcoplaza98 <44683887+marcoplaza98@users.noreply.github.com>
2018-12-18 21:13:25 +0100

    Add files via upload

--- lmc.lisp
+++ lmc.lisp
@@ -1,46 +1,206 @@
 (defun one-instruction (state)
-  (cond 
-  ((= (floor (nth (nth 4 state) (nth 6 state)) 100) 1)  ;add
-		(setf (nth 2 state)(+ (mod (nth (nth 4 state) (nth 6 state)) 100) (nth 2 state)))
-		(cond ((> (nth 2 state) 1000)
-			(setf (nth 2 state)(mod (nth 2 state) 1000))
-			(setf (nth 12 state) T)
-			(T (setf (nth 12 state) nil)))))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 2) ;sub
-		(setf (nth 2 state) (- (nth 2 state) (mod (nth (nth 4 state) (nth 6 state)) 100)))
-		(cond ((< (nth 2 state) 0)
-			(setf (nth 2 state)(mod (nth 2 state) 1000))
-			(setf (nth 12 state) T))
-			(T (setf (nth 12 state) nil))))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 3) ;store
-		(setf (nth (mod (nth (nth 4 state) (nth 6 state)) 100) (nth 6 state)) (nth 2 state)))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 5) ;load
-		(setf (nth 2 state) (nth (mod (nth (nth 4 state) (nth 6 state)) 100) (nth 6 state))))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 6) ;branch
-		(setf (nth 4 state) (mod (nth (nth 4 state) (nth 6 state)) 100)))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 7) ;branch if zero
-		(cond ((and (= (nth 2 state) 0)(null (nth 12 state)))
-			(setf (nth 4 state) (mod (nth (nth 4 state) (nth 6 state)) 100)))))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 8) ;branch if zero
-		(cond ((null (nth 12 state))
-			(setf (nth 4 state) (mod (nth (nth 4 state) (nth 6 state)) 100)))))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 9) ;(nth 8 state) e (nth 10 state)put
-		(cond ((= (floor (nth (nth 4 state) (nth 6 state)) 100) 9)
-			(cond ((= (mod (nth (nth 4 state) (nth 6 state)) 100) 01) ;(nth 8 state)
-				(setf (nth 2 state) (first (nth 8 state)))
-				(setf (nth 8 state) (rest (nth 8 state)))))
-			(cond ((= (mod (nth (nth 4 state) (nth 6 state)) 100) 02) ;(nth 10 state)put
-				(setf (nth 10 state) (insertLast (nth 10 state) (nth 2 state)))))
-				)))
-	((= (floor (nth (nth 4 state) (nth 6 state)) 100) 0) ;halt
-		(return 0))
+  (cond
+	((is-halted-state state)
+		(fail-instruction))
+	((= (fetch-instruction state) 1)  ;add
+		(add-instruction state))
+	((= (fetch-instruction state) 2) ;sub
+		(sub-instruction state))
+	((= (fetch-instruction state) 3) ;store
+		(store-instruction state))
+	((= (fetch-instruction state) 5) ;load
+		(load-instruction state))
+	((= (fetch-instruction state) 6) ;branch
+		(branch-instruction state))
+	((= (fetch-instruction state) 7) ;branch if zero
+		(branchz-instruction state))
+	((= (fetch-instruction state) 8) ;branch if positive
+		(branchp-instruction state))
+	((= (fetch-instruction state) 9) ;input e output
+			(cond ((= (mod (nth (nth 4 state) (nth 6 state)) 100) 01) ;input
+					(if (null (nth 8 state))
+						(fail-instruction)
+						(input-instruction state)))
+				  ((= (mod (nth (nth 4 state) (nth 6 state)) 100) 02) ;output
+					(output-instruction state))))
+	((= (fetch-instruction state) 0) ;halt
+		(halt-instruction state)
 	)
-	(setf (nth 4 state) (1+ (nth 4 state)))
-	state
-)
+	(T
+		(fail-instruction)) ;istruzione non valida
+))
 
 (defun insertLast (lista element)
 	(cond ((null lista)
 		(list element))
 	(T
 		(append lista element))))
+
+(defun fetch-instruction (state)
+	(floor (nth (nth 4 state) (nth 6 state)) 100))
+	
+(defun value-instruction (state)
+	(mod (nth (nth 4 state) (nth 6 state)) 100))
+	
+(defun add-instruction (state)
+	(cond 
+		((> (+ (nth 2 state) (nth (value-instruction state) (nth 6 state))) 1000)
+			((lambda (acc flag pc)
+				(list 'STATE 
+				:acc acc 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag flag ))
+				(mod (+ (nth 2 state) (nth (value-instruction state) (nth 6 state))) 1000) 'flag (1+ (nth 4 state))))
+		(T 
+			((lambda (acc flag pc)
+				(list 'STATE 
+				:acc acc 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag flag ))
+				(+ (nth 2 state) (nth (value-instruction state) (nth 6 state))) 'noflag (1+ (nth 4 state)))
+		)
+	))
+
+(defun sub-instruction (state)
+	(cond 
+		((< (- (nth 2 state) (nth (value-instruction state) (nth 6 state))) 0)
+			((lambda (acc flag pc)
+				(list 'STATE 
+				:acc acc 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag flag ))
+				(mod (- (nth 2 state) (nth (value-instruction state) (nth 6 state))) 1000) 'flag (1+ (nth 4 state))))
+		(T 
+			((lambda (acc flag pc)
+				(list 'STATE 
+				:acc acc 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag flag ))
+				(- (nth 2 state) (nth (value-instruction state) (nth 6 state))) 'noflag (1+ (nth 4 state))))
+		)
+	)
+
+(defun store-instruction (state)
+	(setf (nth (value-instruction state) (nth 6 state)) (nth 2 state))
+	((lambda (pc)
+		(list 'STATE 
+				:acc (nth 2 state) 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag (nth 12 state) ))
+				(1+ (nth 4 state))))
+
+(defun load-instruction (state)
+	((lambda (acc pc)
+		(list 'STATE 
+				:acc acc 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag (nth 12 state) ))
+				(nth (value-instruction state) (nth 6 state)) (1+ (nth 4 state))))
+
+(defun branch-instruction (state)
+	((lambda (pc)
+		(list 'STATE 
+				:acc (nth 2 state) 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag (nth 12 state) ))
+				(value-instruction state)))
+
+(defun branchz-instruction (state)
+	(cond
+		((and (= (nth 2 state) 0) (eq (nth 12 state) 'noflag))
+			((lambda (pc)
+				(list 'STATE 
+					:acc (nth 2 state) 
+					:pc pc 
+					:mem (nth 6 state)
+					:in (nth 8 state)
+					:out (nth 10 state)
+					:flag (nth 12 state) ))
+				(value-instruction state)))
+		(T state)))
+
+(defun branchp-instruction (state)
+	(cond
+		((eq (nth 12 state) 'noflag)
+			((lambda (pc)
+				(list 'STATE 
+					:acc (nth 2 state) 
+					:pc pc 
+					:mem (nth 6 state)
+					:in (nth 8 state)
+					:out (nth 10 state)
+					:flag (nth 12 state) ))
+				(value-instruction state)))
+		(T state)))
+	
+(defun input-instruction(state)
+	((lambda (acc pc in)
+				(list 'STATE 
+				:acc acc 
+				:pc pc 
+				:mem (nth 6 state)
+				:in in
+				:out (nth 10 state)
+				:flag (nth 12 state) ))
+				(first (nth 8 state)) (1+ (nth 4 state)) (rest (nth 8 state))))
+
+(defun output-instruction(state)
+	((lambda (pc out)
+				(list 'STATE 
+				:acc (nth 2 state) 
+				:pc pc 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out out
+				:flag (nth 12 state) ))
+				(1+ (nth 4 state)) (insertLast (nth 10 state) (nth 2 state))))
+
+(defun halt-instruction(state)
+	((lambda ()
+				(list 'HALTED-STATE 
+				:acc (nth 2 state) 
+				:pc  (nth 4 state) 
+				:mem (nth 6 state)
+				:in (nth 8 state)
+				:out (nth 10 state)
+				:flag (nth 12 state) ))
+				))
+
+(defun fail-instruction ()
+	nil)
+
+(defun is-halted-state (state)
+	(if (eq (nth 0 state) 'halted-state)
+		T
+		nil))
+	
+(defun execution-loop (state)
+	(cond 
+		((is-halted-state state)
+			(nth 10 state))
+		((null state)
+			nil)
+		(T
+			(execution-loop (one-instruction state)))))
+	
